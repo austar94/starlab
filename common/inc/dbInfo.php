e6454e4492cd46f314a125cf968ca89f24ef45be
austar94 <48815765+austar94@users.noreply.github.com>
2019-03-22 12:20:32 +0900

    Create dbInfo.php

---
+++ common/inc/dbInfo.php
@@ -0,0 +1,7 @@
+<?php
+$dbHost							=	'localhost';
+$dbName							=	'user DB NAME';
+$dbID							=	'user DBID';
+$dbPWD							=	'user DBPWD';
+$dbPort							=	'3306';               //mysql 기본 포트
+?>
