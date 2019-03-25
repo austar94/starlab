72d36153e00158b2084ee15220271b50fcec6e1e
austar94 <48815765+austar94@users.noreply.github.com>
2019-03-25 15:50:38 +0900

    회원가입 js

---
+++ intro/js/join.js
@@ -0,0 +1,11 @@
+let userIDCheck     = 0;
+let userPWCheck     = 0;
+let userRPWCheck    = 0;
+
+//아이디체크
+$(document).on('chagne', 'input[name="userID"]', function(){
+  userIDCheck       = 0;
+  
+  
+  
+})
