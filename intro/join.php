785beceab473ef7ee669d3953bfeb556f2331e5a
austar94 <48815765+austar94@users.noreply.github.com>
2019-03-25 15:45:34 +0900

    Create join.php

---
+++ intro/join.php
@@ -0,0 +1,12 @@
+
+<input type="text" name="userID">
+<span id="idText"></span>
+<button onclick="checkUserID()">중복확인</button>
+
+<input type="text" name="userPW">
+<span id="pwText"></span>
+
+<input type="text" name="re_userPW">
+<span id="re_pwText"></span>
+
+<script src="/intro/js/join.js"></script>
