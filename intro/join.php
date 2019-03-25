665df38c7bc2724f2f0b8c00c6601b14b11dcc1c
austar94 <48815765+austar94@users.noreply.github.com>
2019-03-25 16:19:01 +0900

    Update join.php

--- intro/join.php
+++ intro/join.php
@@ -1,14 +1,15 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/page/head.php'; ?>
+<body>
+ <input type="text" name="userID">
+  <span id="idText"></span>
+  <button onclick="checkUserID()">중복확인</button>
 
+  <input type="text" name="userPW">
+  <span id="pwText"></span>
 
-<input type="text" name="userID">
-<span id="idText"></span>
-<button onclick="checkUserID()">중복확인</button>
+  <input type="text" name="re_userPW">
+  <span id="re_pwText"></span>
 
-<input type="text" name="userPW">
-<span id="pwText"></span>
-
-<input type="text" name="re_userPW">
-<span id="re_pwText"></span>
-
-<script src="/intro/js/join.js"></script>
+  <script src="/intro/js/join.js"></script>
+</body>
+</html>
