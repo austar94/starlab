9aa8a38d35a3ea483bb764636268348bde9d508b
austar94 <48815765+austar94@users.noreply.github.com>
2019-03-25 16:21:40 +0900

    중복 아이디확인

---
+++ intro/event/join_checkID.php
@@ -0,0 +1,45 @@
+<?php  
+$loginFlag						=	1;
+include $_SERVER['DOCUMENT_ROOT'] . "/common/page/top.php";
+include $_SERVER['DOCUMENT_ROOT'] . "/common/page/jsonLogin.php";
+
+if(!$_POST['userID']){
+  $data						=	array(
+		'errCd'						=>	1,
+		'errMsg'					=>	'아이디를 입력해주시기 바랍니다.'
+	);
+	echo json_encode($data);
+	exit;
+}
+
+if(preg_match('/[^a-z0-9]{6,15}+/', $_POST['userID'])) {
+    $data						=	array(
+		'errCd'						=>	1,
+		'errMsg'					=>	'아이디를 조건이 맞지 않습니다. (영문소문자/숫자, 6~15자)'
+	);
+	echo json_encode($data);
+	exit;
+}
+
+
+$search							=	array(
+	'userID'					  	=>	$_POST['userID'],
+	'userState'						=>	array(1,2,3)
+);
+$msg							  =	$common['MemberManager']->get_userData(1, '', '', $search);
+if($msg->getData()){
+	$data						  =	array(
+		'errCd'						=>	0,
+		'errMsg'					=>	'',
+		'success'					=>	1
+	);
+	echo json_encode($data);
+	exit;
+} else {
+	$data						=	array(
+		'errCd'						=>	1,
+		'errMsg'					=>	'이미 사용중인 아이디입니다.'
+	);
+	echo json_encode($data);
+	exit;
+}
